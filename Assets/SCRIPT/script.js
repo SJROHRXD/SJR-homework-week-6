d0ec8017a3f347609b4714dfa7cf41480d30c0a5
SJROHRXD <85260182+SJROHRXD@users.noreply.github.com>
2021-07-25 18:51:46 -0700

    core functionality, dynamic current weather, building forecast

--- Assets/SCRIPT/script.js
+++ Assets/SCRIPT/script.js
@@ -1,6 +1,13 @@
 // VARIABLES
 let citiesList =[];
 
+var currentDay = moment().format("dddd, MMMM Do, YYYY");
+$("#currentDay").append(currentDay);
+
+var currentTime = moment().format("LT");
+$("#currentTime").append(currentTime);
+
+
 // var maxTempArray = [];
 // var minTempArray = [];
 // var iconArray = [];
@@ -8,50 +15,71 @@ let citiesList =[];
 // var humidArray = [];
 // var windArray = [];
 
+// // ON SUBMIT / BUTTON CLICK, GRABS CITY FROM FORM 🌎☔
 
-
-
-
-// // ON SUBMIT / BUTTON CLICK, GRABS CITY FROM FORM 
 $("#btnSubmit").click(function(event) {
-    event.preventDefault;
+    event.preventDefault();
     var city = $("#city").val();
     var apiKey = "24a0e80c68d5506ba3d18db9845f820f";
-    var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey;
-    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey
+    var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial"+"&appid="+apiKey;
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey;
       
     $.ajax({
         url: currentWeatherURL,
         method: "GET",
-    
-        }).then(function (response) {
-            console.log('AJAX Response \n-------------');
-            console.log(response);
-            // localStorage.setItem something something cities array
+        dataType: "json",
+        success: function(jsonWeather) {
             
-            //append to html elements for the right now weather
-        });
+            
+            console.log (jsonWeather)
+
+        
+            var cityName = $(`<p id='city-name'><i class='fas fa-search-location'></i> ${jsonWeather.name}</p>`);
+            var cityWeather = $(`<p id='city-weather'><i class='fas fa-cloud-sun'></i> ${jsonWeather.weather[0].description}</p>`);
+            var cityTemp = $(`<p id='city-temp'><i class='fas fa-thermometer-quarter'></i> ${jsonWeather.main.temp} F&deg;</p>`);
+
+            $("#cWOPlist").append(cityName, cityWeather, cityTemp)
+            
+        }
+    })
 
     $.ajax({
         url: forecastURL,
         method: "GET",
+        dataType: "json",
+        success: function (jsonForecast) {
 
-    }).then(function(response) {
+            console.log (jsonForecast)
 
-        // for loops holy fuck
+            // if time dt_text is 12:00:00 - compare
 
-        // append to html ellies
+            //main.dt_txt
+            var foreDate = $(`<p id='fore-Date'><i class='fas fa-calendar-week'></i> ${jsonForecast.main.dt_txt}</p>`)
+
+            //main.temp_max 
+            var foreTempHigh = $(`<p id='fore-TempHigh'><i class='fas fa-thermometer-full'></i> ${jsonForecast.main.temp_high} F&deg;</p>`);
             
-            // what time get weather
-            // "5 Day Forecast: #city "
-            // date for 5 days
-                // today (date), day name (date), day name (date), day name (date), day name (date)
-            // icon
-            // description
-            // high temp
-            // low temp
-            // humidity
-            // wind speed
+            //main.temp_low 
+            
+            var foreTempLow = $(`<p id='fore-TempLow'><i class='fas fa-thermometer-empty'></i> ${jsonForecast.main.temp_low} F&deg;</p>`);
+            
+            //main.humidity 
+            var foreHumidity = $(`<p id='fore-Humidity'><i class='fas fa-tint'></i> ${jsonForecast.main.humidity}</p>`);
 
-    }
-    }) // idk something
+            //weather[0].description
+            var foreDescript = $(`<p id='fore-Descript'><i class='fas fa-cloud-sun'></i> ${jsonForecast.weather[0].description}</p>`);
+
+            //sunrise
+            var foreSunrise = $(`<p id='fore-Sunrise'><i class='far fa-sun'></i> ${jsonForecast.sunrise}</p>`);
+            
+            //sunset
+            var foreSunset = $(`<p id='fore-Sunset'><i class='far fa-moon'></i> ${jsonForecast.sunset}</p>`);
+
+            // console.log(foreDate, foreTempHigh, foreTempLow, foreDescript, foreHumidity, foreSunrise, foreSunset);
+
+        }
+    })
+
+
+
+})
