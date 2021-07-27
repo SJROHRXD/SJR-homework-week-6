d7a83ecf96b134f2ca66e6703a6f8f0ba9c3492d
Sarah Jeanne Rohr <85260182+SJROHRXD@users.noreply.github.com>
2021-07-26 20:55:47 -0700

    script attempts

---
+++ Assets/SCRIPT/jankyextras.js
@@ -0,0 +1,82 @@
+// function whenIsIt(jsonForecast) {
+//     console.log (jsonForecast.list.dt_txt.includes('15:00:00'))
+
+    
+// }
+
+
+// var rightNowForeCast = jsonForecast.list.filter (reading => {
+//     return reading.dt_txt.includes=("15:00:00");
+// })
+
+// console.log(rightNowForeCast);
+
+
+
+// then((jsonFork) => {
+//     var weekForecast = jsonFork.list.filter (i => i.dt_txt.includes('12:00:00'));
+//     var day = 0;
+//     var dayofWeekF = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+// weekForecast.forEach((x) =>
+// {
+//     var newD = new Date(x.dt_txt);
+//     console.log(newDate);
+
+
+// for (i = 0; i < items.list.length; i++){
+//     var item = items.list[i].dt
+//     var dateFC = new Date(item*1000)
+//     var dateNumberFC = date.getDate()
+//     var date_text = items.list[i].dt_txt
+// }
+// var buildFClist = list.filter(item => item.dt_txt.includes("12:00:00"))
+
+
+
+            
+// var filteredForecast = ${jsonForecast.list[0].dt_txt}.filter((data) => {
+//     return data.list[0].dt_txt = '15:00:00'
+// })
+// console.log(filteredForecast)
+
+
+            // console.dir (jsonForecast.list);
+            
+            // var jsonFCArray = Array.prototype.slice.call(jsonForecast.list);
+            // console.log (jsonFCArray);
+
+            // var filterFC = jsonFCArray.filter((item) => {
+            //     return item.list = "12:00:00"
+            // })
+
+            // console.log(filterFC)
+
+            // for (i = 0; i < jsonForecast.list.length; i =+ 8)
+
+                        // var jsonFCArray = Array.prototype.slice.call(jsonForecast.list);
+            
+            // function iterate(item) {
+            //     console.log(item);
+            // }
+            // Array.prototype.forEach.call(jsonFCArray, iterate);
+            
+            // jsonFCArray.forEach(function(timeThing) {
+            //     let timeThing = jsonForecast.list.dt_txt
+            //     if (timeThing includes)
+            // })
+            
+            // console.table(jsonForecast.list.[])
+
+            // Array.prototype.forEach((list, dt_txt) => console.log())
+
+
+            // newForecast = jsonForecast.filter(function())
+
+
+            // var maxTempArray = [];
+// var minTempArray = [];
+// var iconArray = [];
+// var descArray = [];
+// var humidArray = [];
+// var windArray = [];
